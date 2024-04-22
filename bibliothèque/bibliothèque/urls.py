734f38de0526dd12ca9f2f7b73cd29f8681ad731
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-04-22 09:45:31 -0400

    😌done with urls

--- bibliothèque/bibliothèque/urls.py
+++ bibliothèque/bibliothèque/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from livres import views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name='home'),
+    path('livre/<int:livre_id>/', views.livre, name='livre'),
+    path('ajouter/', views.ajout_livre, name='ajout_livre'),
+    path('modifier/<int:livre_id>/', views.modifier_livre, name='modifier_livre'),
+    path('supprimer/<int:livre_id>/', views.supprimer_livre, name='supprimer_livre'),
 ]
--- bibliothèque/livres/views.py
+++ bibliothèque/livres/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+
+
+# on ajoute les methodes pour les urls afin que le render des pages html puisse se faire 
 # Create your views here.
+def home(request):
+    # Logique pour récupérer la liste de tous les livres disponibles depuis la base de données
+  
+    return render(request, 'index.html' )
+
+def livre(request ):
+    # Logique pour récupérer les détails d'un livre spécifique en fonction de son ID
+ 
+    return render(request, 'livre.html', {'livre': livre})
+
+def ajout_livre(request):
+    # Logique pour afficher le formulaire pour ajouter un nouveau livre
+    return render(request, 'ajoutlivre.html')
+
+def modifier_livre(request ):
+    # Logique pour afficher le formulaire pour modifier les informations d'un livre existant
+ 
+    return render(request, 'modifierlivre.html', {'livre': livre})
+
+def supprimer_livre(request):
+    # Logique pour afficher la page de confirmation de suppression d'un livre
+   
+    return render(request, 'supprimer_livre.html', {'livre': livre})
