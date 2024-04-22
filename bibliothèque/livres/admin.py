c0f6192e7cd3fa11304ad3809230c95437e58dad
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-04-22 13:19:06 -0400

    FINI 😎

--- bibliothèque/livres/admin.py
+++ bibliothèque/livres/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Livres
+
+class affichagetable(admin.ModelAdmin):
+    list_display= ["titre","auteur","date_publication","genre"]
+admin.site.register(Livres,affichagetable)
+
 # Register your models here.
